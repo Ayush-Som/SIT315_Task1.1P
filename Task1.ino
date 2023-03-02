c7d73048de8a6199daea51b9e5e86bb272da9bff
Ayush-Som <103960059+Ayush-Som@users.noreply.github.com>
2023-03-02 12:01:07 +0530

    Update Task1.ino

--- Task1.ino
+++ Task1.ino
@@ -1,18 +1,24 @@
+//PIR pin define 
 int PIR = 4;
+// PIR reading variable 
 int value = 0;
 
 void setup()
 {
+  //Initializing Serial Monitor 
   Serial.begin(9600);
+  //Initializing BUILT-IN led and PIR sensor 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PIR, INPUT);
 }
 
 void loop()
 {
+  // Storing data from PIR sensor to value 
   value = digitalRead(PIR);
   if (value)
   {
+    //BUILT-IN LED working as an actuator 
     digitalWrite(LED_BUILTIN, 1);
     Serial.println("Motion Detected");
   }
